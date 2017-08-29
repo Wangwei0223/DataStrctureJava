5d910c5b1b4b625bbfb15a232dfb2563c7229338
WWWaeing <291978313@qq.com>
2017-08-29 23:18:19 +0800

    Add files via upload

---
+++ AddTwoNumber.java
@@ -0,0 +1,27 @@
+/**
+ * Created by wangwei on 17-8-24.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode {
+      private int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
+
+
+class AddTwoNumber {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+}
